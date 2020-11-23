5c400f5469846ad11229e7a35ea546622b8f8337
Samuel <silvasmlsss.02@gmail.com>
2020-11-22 21:29:17 -0300

    Site em Andamento

---
+++ Pagina0/pag.js
@@ -0,0 +1,5 @@
+const 
+var
+function name(params) {
+    
+}
--- Pagina0/pag0.js
+++
@@ -1,8 +0,0 @@
-/*
-function acao(){
-    document.querySelector("h1").textContent = "Good Morning";
-}
-*/
-
-var titulo = document.getElementById("home");
-titulo.onmousemove = "Samuel"
